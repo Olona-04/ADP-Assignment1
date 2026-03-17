207066f017104951fce3cd01cbd2bbe75e1cc342
Zahrah Vermaak <221406395@mycput.ac.za>
2026-03-18 01:09:36 +0200

    Added code to User model class using builder pattern

---
+++ src/main/java/util/Main.java
@@ -0,0 +1,16 @@
+package util;
+
+import Domain.User;
+
+public class Main {
+    public static void main(String[] args) {
+
+       // User user1 = new User.Builder()
+         //       .setUserId("User13456")
+           //     .setName("Jane")
+             //   .setSurname("Doe")
+               // .setEmail("JaneDoes@gmail.com")
+                //.build();
+        //System.out.println(user1.getFullName());
+    }
+}
