c96aa1524cdcc0f311f723d8fef09616ecf9c4f2
Zahrah Vermaak <221406395@mycput.ac.za>
2026-03-23 23:44:39 +0200

    Added PaymentFactory and AddressFactory without testing

--- src/main/java/za/ac/cput/Factory/UserFactory.java
+++ src/main/java/za/ac/cput/Factory/UserFactory.java
@@ -3,7 +3,6 @@ package za.ac.cput.Factory;
 import za.ac.cput.Domain.User;
 
 public class UserFactory {
-
         public static User createUser(String id, String name, String surname, String email) {
             return new User.Builder()
                     .setUserId(id)
